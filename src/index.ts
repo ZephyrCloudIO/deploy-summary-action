e74ceb200d8bb1637f1d848b66e62d2d00197ca5
Oleksandr Pavlovskyi <opavlovskyi@gmail.com>
2024-10-31 16:08:52 +0200

    test action summary

---
+++ src/index.ts
@@ -0,0 +1,29 @@
+import { getInput, setOutput, setFailed, summary } from '@actions/core';
+import { context, getOctokit } from '@actions/github';
+
+try {
+  const createJobSummary = async (nameToGreet: string, time: string, payload: string) => {
+    await summary.addRaw(`Hello ${nameToGreet}!`);
+    await summary.addEOL();;
+    const tableData = [
+      [{ data: 'Name to Great'}, { data: nameToGreet}],
+      [{ data: 'Time'}, { data: time}],
+      [{ data: 'Payload' }, { data: payload}],
+    ]
+    await summary.addTable(tableData);
+  };
+
+  (async () => {
+    // `who-to-greet` input defined in action metadata file
+    const nameToGreet = getInput('who-to-greet');
+    console.log(`Hello ${nameToGreet}!`);
+    const time = (new Date()).toTimeString();
+    setOutput("time", time);
+    // Get the JSON webhook payload for the event that triggered the workflow
+    const payload = JSON.stringify(context.payload, undefined, 2)
+    console.log(`The event payload: ${payload}`);
+    await createJobSummary(nameToGreet, time, payload);
+  })();
+} catch (error) {
+  setFailed(error.message);
+}
