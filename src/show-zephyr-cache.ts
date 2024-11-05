613f1c26e77705748ee508327a3f24ca99a6676b
Oleksandr Pavlovskyi <opavlovskyi@gmail.com>
2024-11-05 16:38:40 +0200

    debug

--- src/show-zephyr-cache.ts
+++ src/show-zephyr-cache.ts
@@ -7,7 +7,18 @@ const ZEPHYR_FOLDER = '.zephyr';
 export const showZephyrCache = async () => {
   const dirPath = path.resolve(homedir(), ZEPHYR_FOLDER);
   console.log('Zephyr cache in %s:', dirPath);
-  fs.readdirSync(dirPath).forEach(file => {
+  const dir = fs.readdirSync(dirPath);
+  dir.forEach(file => {
     console.log(file);
+  });
+  dir.forEach(file => {
+    console.log('Reading file "%s"', file);
+    const filePath = path.resolve(dirPath, file);
+    const content = fs.readFileSync(filePath, 'utf-8');
+    const json = JSON.parse(content);
+    if (json.key?.startsWith('ze-app-deploy-result')) {
+      console.log('Deploy result for %s:', json.key);
+      console.log(json.value);
+    }
   });
 };
