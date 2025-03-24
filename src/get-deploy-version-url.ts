a289f1cd624657a7daeff9f6c532235f4125cf84
Rodrigo Yokota <ryok90@gmail.com>
2025-03-24 16:27:14 -0300

    fix: access zephyr cache directly and move from npm to pnpm

--- src/get-deploy-version-url.ts
+++ src/get-deploy-version-url.ts
@@ -1,6 +1,29 @@
-import { appDeployResultCache } from 'zephyr-edge-contract';
+import persist from 'node-persist';
+import { homedir } from 'node:os';
+import { join } from 'node:path';
 
-export const getDeployVersionUrl = async (application_uid: string) => {
-  const result = await appDeployResultCache.getAppDeployResult(application_uid);
-  return result?.urls[0];
+export interface Deployed {
+  app: string;
+  url: string;
+}
+
+export const getDeployVersionUrl = async (
+  application_uid: string,
+): Promise<string | undefined> => {
+  const deployed = await getDeployedApps();
+
+  return deployed.find(({ app }) => app === application_uid)?.url;
+};
+
+const getDeployedApps = async (): Promise<Deployed[]> => {
+  await persist.init({ dir: join(homedir(), '.zephyr') });
+
+  const deployed: Deployed[] = [];
+  await persist.forEach(async ({ key, value }) => {
+    if (!key.startsWith('ze-app-deploy-result:')) return;
+    deployed.push({ app: key.split(':')[1].split('.')[0], url: value.urls[0] });
+  });
+  deployed.sort((a, b) => (a.app > b.app ? 1 : -1));
+
+  return deployed;
 };
